c95f22c6c793dc7bbca93beacb67c597282e20e1
Xiao Tong <tongxiao@uw.edu>
2015-04-17 15:19:40 -0700

    People table view

---
+++ SkillSwap/PeopleDetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  PeopleDetailViewController.h
+//  SkillSwap
+//
+//  Created by Xiao Tong on 4/17/15.
+//  Copyright (c) 2015 Chen Zhu. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface PeopleDetailViewController : UIViewController
+
+@end
---
+++ SkillSwap/PeopleDetailViewController.m
@@ -0,0 +1,26 @@
+//
+//  PeopleDetailViewController.m
+//  SkillSwap
+//
+//  Created by Xiao Tong on 4/17/15.
+//  Copyright (c) 2015 Chen Zhu. All rights reserved.
+//
+
+#import "PeopleDetailViewController.h"
+@interface PeopleDetailViewController()
+
+@end
+@implementation PeopleDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
