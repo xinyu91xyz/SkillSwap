4aba0a68dbd0254dffe5753a54f1a83dceac2eb6
Chen Zhu <zhuchen@uw.edu>
2015-04-17 18:12:36 -0700

    SkillsLists

--- SkillSwap/ProfileTableViewController.m
+++ SkillSwap/ProfileTableViewController.m
@@ -23,31 +23,25 @@
 
 @implementation ProfileTableViewController
 
-- (id)initWithStyle:(UITableViewStyle)style{
-    self = [super initWithStyle:style];
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
     if (self) {
-//        PFQuery *queryUser = [PFQuery queryWithClassName:@"User"];
-//        [queryUser whereKey:@"objectId" equalTo:[PFUser currentUser]];
-//        [queryUser findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-//            if (!error) {
-//                PFObject *user= objects[0];
-//                userEmail = user[@"email"];
-//                userRealName = user[@"realName"];
-//            } else {
-//                
-//            }
-//        }];
-        
         
     }
     return self;
 }
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    PFRelation *knownRelation = [[PFUser currentUser] relationForKey:@"knownSkills"];
+    knownSkills = [[knownRelation query] findObjects];
     
-    // animals = @[@"Bear", @"Black Swan", @"Buffalo", @"Camel", @"Cockatoo", @"Dog", @"Donkey", @"Emu", @"Giraffe", @"Greater Rhea", @"Hippopotamus", @"Horse", @"Koala", @"Lion", @"Llama", @"Manatus", @"Meerkat", @"Panda", @"Peacock", @"Pig", @"Platypus", @"Polar Bear", @"Rhinoceros", @"Seagull", @"Tasmania Devil", @"Whale", @"Whale Shark", @"Wombat"];
+    PFRelation *toLearnRelation = [[PFUser currentUser] relationForKey:@"toLearnSkills"];
+    toLearnSkills = [[toLearnRelation query] findObjects];
+    
     
     animals = @{@"B" : @[@"Bear", @"Black Swan", @"Buffalo"],
                 @"C" : @[@"Camel", @"Cockatoo"],
@@ -69,29 +63,10 @@
     
     
     PFUser *user = [PFUser currentUser];
-        
+    
     userEmail = [user objectForKey:@"email"];
     userRealName = [user objectForKey:@"realName"];
     
-    PFQuery *queryKnown = [PFQuery queryWithClassName:@"UserSkill"];
-    [queryKnown whereKey:@"userId" equalTo:[PFUser currentUser]];
-    [queryKnown whereKey:@"skillType" equalTo:@"known"];
-    [queryKnown findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (!error) {
-            knownSkills = [[NSArray alloc] initWithArray:objects];
-        } else {
-        }
-    }];
-    
-    PFQuery *queryToLearn = [PFQuery queryWithClassName:@"UserSkill"];
-    [queryToLearn whereKey:@"userId" equalTo:[PFUser currentUser]];
-    [queryToLearn whereKey:@"skillType" equalTo:@"tolearn"];
-    [queryToLearn findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (!error) {
-            toLearnSkills = [[NSArray alloc] initWithArray:objects];
-        } else {
-        }
-    }];
     
     
     
@@ -102,37 +77,11 @@
     }
     
     for (PFObject *object in toLearnSkills) {
-        NSLog(@"toLearnSkill: %@",object[@"skillName"]);
+        NSLog(@"%@",object[@"skillType"]);
     }
     
 }
-//
-//- (NSString *)getImageFilename:(NSString *)animal
-//{
-//    NSString *imageFilename = [[animal lowercaseString] stringByReplacingOccurrencesOfString:@" " withString:@"_"];
-//    imageFilename = [imageFilename stringByAppendingString:@".jpg"];
-//    
-//    return imageFilename;
-//}
 
-//- (PFQuery *) queryForTable {
-//    PFQuery *query = [PFQuery queryWithClassName:@"User"];
-//    [query whereKey:@"objectId" equalTo:[PFUser currentUser]];
-//    return query;
-//}
-
-/*!
- Called when objects have loaded from Parse. If you override this method, you must
- call [super objectsDidLoad:] in your implementation.
- @param error The Parse error from running the PFQuery, if there was any.
- */
-//- (void)objectsDidLoad:(PFUI_NULLABLE NSError *)error {
-//    [super objectsDidLoad:error];
-//    NSString *userEmail = [[self.objects objectAtIndex:0] objectForKey:@"email"];
-//    NSLog(@"%@",userEmail);
-//    NSLog(@"objectsDidLoad");
-//}
-//
 
 - (void)didReceiveMemoryWarning
 {
@@ -145,32 +94,69 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     // Return the number of sections.
-    return [animalSectionTitles count];
+    return 3;
+    //    return [animalSectionTitles count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    NSString *sectionTitle = [animalSectionTitles objectAtIndex:section];
-    NSArray *sectionAnimals = [animals objectForKey:sectionTitle];
-    return [sectionAnimals count];
+    //    NSString *sectionTitle = [animalSectionTitles objectAtIndex:section];
+    //    NSArray *sectionAnimals = [animals objectForKey:sectionTitle];
+    //    return [sectionAnimals count];
+    if (section == 0) return 1;
+    else if (section == 1) return [knownSkills count];
+    else if (section == 2) return [toLearnSkills count];
+    else return 0;
+}
+
+- (CGFloat) tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    if (section == 0)
+        return 1.0f;
+    return 32.0f;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
-    return [animalSectionTitles objectAtIndex:section];
+    
+    if (section == 1) {
+        return @"My Skills";
+    }
+    else if (section == 2) {
+        return @"Want to Learn";
+    }
+    else {
+        return @"";
+    }
+    //    return [animalSectionTitles objectAtIndex:section];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UserProfileCell" forIndexPath:indexPath];
-    
+    UITableViewCell *cell;
+    if (indexPath.section == 0) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"UserProfileCell" forIndexPath:indexPath];
+    }
+    else if (indexPath.section == 1) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"MySkillCell" forIndexPath:indexPath];
+        
+        cell.textLabel.text = knownSkills[indexPath.row][@"skillName"];
+        
+    }
+    else if (indexPath.section == 2) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"ToLearnCell" forIndexPath:indexPath];
+        cell.textLabel.text = toLearnSkills[indexPath.row][@"skillName"];
+        
+    } else {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"UserProfileCell" forIndexPath:indexPath];
+    }
     // Configure the cell...
-    NSString *sectionTitle = [animalSectionTitles objectAtIndex:indexPath.section];
-    NSArray *sectionAnimals = [animals objectForKey:sectionTitle];
-    NSString *animal = [sectionAnimals objectAtIndex:indexPath.row];
-    cell.textLabel.text = animal;
-//    cell.imageView.image = [UIImage imageNamed:[self getImageFilename:animal]];
+    //    NSString *sectionTitle = [animalSectionTitles objectAtIndex:indexPath.section];
+    //    NSArray *sectionAnimals = [animals objectForKey:sectionTitle];
+    //    NSString *animal = [sectionAnimals objectAtIndex:indexPath.row];
+    //    cell.textLabel.text = animal;
+    //    cell.imageView.image = [UIImage imageNamed:[self getImageFilename:animal]];
     
     return cell;
 }
